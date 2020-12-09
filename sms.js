07746781ad9ebe48e8740340994bf6f914497c18
sabahmemon <75626729+sabahmemon86@users.noreply.github.com>
2020-12-09 20:52:23 +0500

    Add files via upload

---
+++ sms.js
@@ -0,0 +1,133 @@
+//******************* School Module System (module 1)****************//
+// var sms =[
+//     { id : 204 , name : "name = Sabah " , lname : " lname = Khalil Ahmed" , class : " class = 8 " , lastClass : " lastclass = 7 " , rollno : " rollno = 23 " , lastPassingMarks : " lastPassingMarks = 768 "}, 
+//     { id : 238, name : "name =  Sara " , lname : " lname = asad " , class : " class = 2 " , lastClass : " lastclass = 1 " , rollno : " rollno = 09 " , lastPassingMarks : " lastPassingMarks = 658 "},
+//     { id : 104 , name : " name = waris " , lname : " lname = Zaib" , class : " class = 8 " , lastClass : " lastclass = 7 " , rollno : "rollno = 54 " , lastPassingMarks : " lastPassingMarks = 688 "},
+//     { id : 160 , name : " name = Shahzaib" , lname : " lname = Ahmed" , class : " class = 4 " , lastClass : " lastclass = 3 " , rollno : " rollno = 19 " , lastPassingMarks : " lastPassingMarks = 726 "},
+//     { id : 192 , name : " name = Aurangzaib" , lname : " lname = Memon" , class : " class = 7 " , lastClass : "lastclass = 6 " , rollno : " rollno = 08 " , lastPassingMarks : " lastPassingMarks = 650 "},
+//     { id : 304 , name : " name = Anus" , lname : "lname = Ahmed" , class : " class = 10 " , lastClass : "lastclass = 9 " , rollno : "rollno = 44 " , lastPassingMarks : " lastPassingMarks = 710 "},
+// ];
+// var student = sms.map(function(sms){
+//     if (sms.id === 204)
+// return console.log(sms.name+sms.rollno+sms.lastClass+sms.class);
+// })
+    // var smsids = sms.map(function(sms){
+    //    return console.log(sms.id);
+    // })
+
+    // var smsnames = sms.map(function(sms){
+    //     return console.log(sms.name);
+    // })
+
+    // var smslname = sms.map(function(sms){
+    //     return console.log(sms.lname);
+    //  })
+
+    //  var smsrollnos = sms.map(function(sms){
+    //     return console.log(sms.rollno);
+    //  })
+    //  var smslastClasss = sms.map(function(sms){
+    //     return console.log(sms.lastClass);
+    // })
+    // var smsclasss = sms.map(function(sms){
+    //     return console.log(sms.class);
+    // })
+    // var smslastPassingMarkss = sms.map(function(sms){
+    //     return console.log(sms.lastPassingMarks);
+    //  })
+//     var student = sms.map(function(sms){
+//         if (sms.id === 204)
+// return console.log(sms.name+sms.rollno+sms.lastClass+sms.class);
+//     })
+
+
+//******************* School Module System (module 2)****************//
+
+// var m2 = [
+//     {AdmissionFees :  'A/F =  25000' , MonthlyFees :  ' m/f = 2500' , ExtracircularFees : ' E/F = 500' , GamingFees : 'G/F = 1000'}, 
+//     {AdmissionFees : 'A/F =20000' , MonthlyFees :  ' m/f =2000' , ExtracircularFees : ' E/F =400' , GamingFees : 'G/F = 900'},
+//     {AdmissionFees : 'A/F =18000' , MonthlyFees :  ' m/f =1900' , ExtracircularFees : ' E/F =400' , GamingFees : 'G/F = 800'},
+//     {AdmissionFees : 'A/F =17000' , MonthlyFees :  ' m/f =1800' , ExtracircularFees : ' E/F =350' , GamingFees : '700'}
+// ];
+// var sms = m2.map(function(m2){
+//        return console.log(m2.MonthlyFees);
+//     });
+// var sms = m2.map(function(m2){
+//         return console.log(m2.AdmissionFees);
+//     });
+//  var sms = m2.map(function(m2){
+//         return console.log(m2.ExtracircularFees);
+//     });
+// var sms = m2.map(function(m2){
+//         return console.log(m2.GamingFees);
+//     });
+// var student = m2.map(function (m2) {
+//      return console.log(m2.AdmissionFees + m2.MonthlyFees + m2.ExtracircularFees + m2.GamingFees);
+// });
+        
+//******************* School Module System (module 3)****************//
+
+// var m3 =[
+//     {sub : " english " , marks : " 44 "},
+//     {sub : " physics " , marks : " 55 "},
+//     {sub : " maths " , marks : " 76 "},
+//     {sub : " chemistry " , marks : " 49 "},
+//     {sub : " biology " , marks :  "60 "}   
+// ]
+
+// var sms = m3.map(function(m3){
+//     return console.log(m3.sub);
+// })
+// var sms = m3.map(function(m3){
+//     return console.log(m3.marks);
+// })
+// var sms = m3.map(function(m3){
+//     if ( m3.marks > 49){
+//         console.log(" pass" + m3.sub);
+//     }
+//         else console.log( " fail " + m3.sub); 
+    
+// })
+
+//******************* School Module System (module 1)****************//
+// var sms =[
+//     { id : 204 , name : "name = Sabah " , lname : " lname = Khalil Ahmed" , class : " class = 8 " , lastClass : " lastclass = 7 " , rollno : " rollno = 23 " , lastPassingMarks : " lastPassingMarks = 768 "}, 
+//     { id : 238, name : "name =  Sara " , lname : " lname = asad " , class : " class = 2 " , lastClass : " lastclass = 1 " , rollno : " rollno = 09 " , lastPassingMarks : " lastPassingMarks = 658 "},
+//     { id : 104 , name : " name = waris " , lname : " lname = Zaib" , class : " class = 8 " , lastClass : " lastclass = 7 " , rollno : "rollno = 54 " , lastPassingMarks : " lastPassingMarks = 688 "},
+//     { id : 160 , name : " name = Shahzaib" , lname : " lname = Ahmed" , class : " class = 4 " , lastClass : " lastclass = 3 " , rollno : " rollno = 19 " , lastPassingMarks : " lastPassingMarks = 726 "},
+//     { id : 192 , name : " name = Aurangzaib" , lname : " lname = Memon" , class : " class = 7 " , lastClass : "lastclass = 6 " , rollno : " rollno = 08 " , lastPassingMarks : " lastPassingMarks = 650 "},
+//     { id : 304 , name : " name = Anus" , lname : "lname = Ahmed" , class : " class = 10 " , lastClass : "lastclass = 9 " , rollno : "rollno = 44 " , lastPassingMarks : " lastPassingMarks = 710 "},
+// ];
+
+// let reformatedarray = sms.map(obj =>{
+//     let robj = {}
+//     robj[obj.id] = obj.name + obj.lname + obj.class + obj.lastClass + obj.rollno + obj.lastPassingMarks
+//     return console.log(robj);
+// })
+//**********************School Module System (module 2)*************//
+// var m2 = [
+//         {AdmissionFees :  'A/F =  25000' , MonthlyFees :  ' m/f = 2500' , ExtracircularFees : ' E/F = 500' , GamingFees : 'G/F = 1000'}, 
+//         {AdmissionFees : 'A/F =20000' , MonthlyFees :  ' m/f =2000' , ExtracircularFees : ' E/F =400' , GamingFees : 'G/F = 900'},
+//         {AdmissionFees : 'A/F =18000' , MonthlyFees :  ' m/f =1900' , ExtracircularFees : ' E/F =400' , GamingFees : 'G/F = 800'},
+//         {AdmissionFees : 'A/F =17000' , MonthlyFees :  ' m/f =1800' , ExtracircularFees : ' E/F =350' , GamingFees : '700'}
+//     ];
+//     let reformatedarray = m2.map(obj => {
+//         let robj = {}
+//          robj[obj.AdmissionFees] = obj.MonthlyFees + obj.ExtracircularFees + obj.GamingFees
+//             return console.log(robj);
+//         });
+
+//******************* School Module System (module 3)****************//
+
+// var m3 =[
+//     {sub : " english " , marks : " 44 "},
+//     {sub : " physics " , marks : " 55 "},
+//     {sub : " maths " , marks : " 76 "},
+//     {sub : " chemistry " , marks : " 49 "},
+//     {sub : " biology " , marks :  "60 "}   
+// ]
+// let reformatedarray = m3.map(obj =>{
+//         let robj = {}
+//         robj[obj.sub] = obj.marks
+//         return console.log(robj);
+//          });
